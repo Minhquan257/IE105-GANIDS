2cf669205cc37ba86b16e70dacda8de033410ba6
Minhquan257 <denonxskt@gmail.com>
2022-12-21 20:16:16 +0700

    update

--- NSL-KDD/spocuGan.py
+++ NSL-KDD/spocuGan.py
@@ -1,6 +1,9 @@
 import tensorflow as tf  # TF 2.0
 import numpy as np
-import time, datetime, os, pickle
+import time
+import datetime
+import os
+import pickle
 from collections import defaultdict
 from matplotlib import pyplot as plt
 from scipy.stats import norm
@@ -14,34 +17,39 @@ from utils import preprocessing
 from utils import utils
 from models import classifiers as clf
 
+
 def plot_kl(kl_history):
-    n = np.arange(0,len(kl_history),1)
+    n = np.arange(0, len(kl_history), 1)
     print(f"kl_divergence len {len(kl_history)}")
 
     fig, ax = plt.subplots()
-    ax.plot(n, kl_history,label='KL',linewidth=2,color="black")
+    ax.plot(n, kl_history, label='KL', linewidth=2, color="black")
     # ax.legend(loc=0,prop={'size': 10})
-    ax.set_ylabel('KL-Divergence',fontsize=11.0)
-    ax.set_xlabel('Epoch',fontsize=11.0)
+    ax.set_ylabel('KL-Divergence', fontsize=11.0)
+    ax.set_xlabel('Epoch', fontsize=11.0)
     ax.tick_params(labelsize=11)
 
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-    fig.savefig('./imgs/nsl_relu_kl_div_plot.png', dpi = 350, bbox_inches='tight')
+    fig.savefig('./imgs/nsl_relu_kl_div_plot.png',
+                dpi=350, bbox_inches='tight')
     fig.savefig('./imgs/nsl_relu_kl_div_plot.pdf', bbox_inches='tight')
-    fig.savefig('./imgs/nsl_relu_kl_div_plot.eps',format='eps')
+    fig.savefig('./imgs/nsl_relu_kl_div_plot.eps', format='eps')
+
 
 def h_function(value):
     h_ = value
     clip_val_max = tf.math.reduce_max(tf.math.reduce_max(h_))
-    h_ = tf.clip_by_value(h_,0,clip_val_max)
-    h_ = pow(h_,3)*(pow(h_,5)-(2*pow(h_,4))+2)
+    h_ = tf.clip_by_value(h_, 0, clip_val_max)
+    h_ = pow(h_, 3)*(pow(h_, 5)-(2*pow(h_, 4))+2)
     return h_
+
 
 def h2_function(value):
     if value > 0:
-        return pow(value,3)*(pow(value,5)-(2*pow(value,4))+2)
+        return pow(value, 3)*(pow(value, 5)-(2*pow(value, 4))+2)
     else:
         return 0
+
 
 def SPOCU_f(input):
     alpha = 3.0937
@@ -49,6 +57,7 @@ def SPOCU_f(input):
     gamma = 4.437
     out = alpha*h_function((input/gamma)+beta) - alpha*h2_function(beta)
     return out
+
 
 class SPOCU(Activation):
 
@@ -58,6 +67,7 @@ class SPOCU(Activation):
 
 
 get_custom_objects().update({'spocu': SPOCU(SPOCU_f)})
+
 
 class Generator(tf.keras.Model):
     def __init__(self):
@@ -75,6 +85,7 @@ class Generator(tf.keras.Model):
         x = self.dense_4(x)
         return x
 
+
 class Discriminator(tf.keras.Model):
     def __init__(self):
         super(Discriminator, self).__init__()
@@ -90,16 +101,19 @@ class Discriminator(tf.keras.Model):
         x = self.dense_3(x)
         return self.dense_4(x)
 
+
 def discriminator_loss(loss_object, real_output, fake_output):
     real_loss = loss_object(tf.ones_like(real_output), real_output)
     fake_loss = loss_object(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
     return total_loss
 
+
 def generator_loss(loss_object, fake_output):
     return loss_object(tf.ones_like(fake_output), fake_output)
 
-def calculate_kl_div(generator_model,noise,y_labels,norm_p):
+
+def calculate_kl_div(generator_model, noise, y_labels, norm_p):
     """
     calculate Kullback–Leibler divergence between the generated dataset and original dataset.
     Source : https://en.wikipedia.org/wiki/Kullback–Leibler_divergence
@@ -107,11 +121,12 @@ def calculate_kl_div(generator_model,noise,y_labels,norm_p):
     g_z = generator_model(noise, y_labels)
 
     q = norm.pdf(g_z.numpy().T)
-    norm_q = q/q.sum(axis=1,keepdims=1)
+    norm_q = q/q.sum(axis=1, keepdims=1)
 
-    kl = np.sum(np.where(norm_p != 0, norm_p * np.log(norm_p/norm_q),0))
+    kl = np.sum(np.where(norm_p != 0, norm_p * np.log(norm_p/norm_q), 0))
 
     return kl
+
 
 def main(arg):
     # settting hyperparameter
@@ -119,26 +134,28 @@ def main(arg):
     epochs = 100
     batch_size = 128
 
-    #-------------------- Load Data & Preprocess ---------------------#
-    train,test, label_mapping = preprocessing.get_data(encoding="Label")
-    data_cols = list(train.columns[ train.columns != 'label' ])
+    # -------------------- Load Data & Preprocess ---------------------#
+    train, test, label_mapping = preprocessing.get_data(encoding="Label")
+    data_cols = list(train.columns[train.columns != 'label'])
 
-    #Remove contant values with a threshold
-    to_drop = preprocessing.get_contant_featues(train,data_cols,threshold=0.995)
+    # Remove contant values with a threshold
+    to_drop = preprocessing.get_contant_featues(
+        train, data_cols, threshold=0.995)
 
-    train.drop(to_drop, axis=1,inplace=True)
-    test.drop(to_drop, axis=1,inplace=True)
+    train.drop(to_drop, axis=1, inplace=True)
+    test.drop(to_drop, axis=1, inplace=True)
 
-    #Normalize row-wise the data (to unit norm) and scale column-wise
-    data_cols = list(train.columns[train.columns != 'label' ])
-    train = preprocessing.normalize_data(train,data_cols)
-    test = preprocessing.normalize_data(test,data_cols)
-    x_train , x_test = preprocessing.preprocess(train,test,data_cols,"Robust",True)
-    data_cols = list(x_train.columns[x_train.columns != 'label' ])
+    # Normalize row-wise the data (to unit norm) and scale column-wise
+    data_cols = list(train.columns[train.columns != 'label'])
+    train = preprocessing.normalize_data(train, data_cols)
+    test = preprocessing.normalize_data(test, data_cols)
+    x_train, x_test = preprocessing.preprocess(
+        train, test, data_cols, "Robust", True)
+    data_cols = list(x_train.columns[x_train.columns != 'label'])
 
     train, test = None, None
     y_train = x_train.label.values
-    y_test = x_test.label.values.reshape(-1,1).astype(np.float32)
+    y_test = x_test.label.values.reshape(-1, 1).astype(np.float32)
 
     att_ind = np.where(x_train.label != label_mapping["normal"])[0]
     for_test = np.where(x_test.label != label_mapping["normal"])[0]
@@ -148,12 +165,13 @@ def main(arg):
 
     x = x_train[data_cols].values[att_ind]
     y = y_train[att_ind]
-    y = y.reshape(-1,1).astype(np.float32)
+    y = y.reshape(-1, 1).astype(np.float32)
     x_train, y_train = None, None
     p = norm.pdf(x.T)
-    norm_p = p/p.sum(axis=1,keepdims=1)
+    norm_p = p/p.sum(axis=1, keepdims=1)
 
-    train_dataset = tf.data.Dataset.from_tensor_slices((tf.convert_to_tensor(x),y)).batch(batch_size, drop_remainder=True)
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.convert_to_tensor(x), y)).batch(batch_size, drop_remainder=True)
 
     generator = Generator()
     discriminator = Discriminator()
@@ -169,30 +187,33 @@ def main(arg):
     D_train_loss = tf.keras.metrics.Mean('D_train_loss', dtype=tf.float32)
 
     @tf.function
-    def train_step(X,y_label):
+    def train_step(X, y_label):
         noise = tf.random.normal([batch_size, latent_dim])
 
         with tf.GradientTape(persistent=True) as tape:
             generated_samples = generator(noise, y_label)
 
-            real_output = discriminator(X,y_label)
+            real_output = discriminator(X, y_label)
             generated_output = discriminator(generated_samples, y_label)
 
             gen_loss = generator_loss(cross_entropy, generated_output)
-            disc_loss = discriminator_loss(cross_entropy, real_output, generated_output)
+            disc_loss = discriminator_loss(
+                cross_entropy, real_output, generated_output)
 
         grad_disc = tape.gradient(disc_loss, discriminator.trainable_variables)
         grad_gen = tape.gradient(gen_loss, generator.trainable_variables)
 
-        disc_optimizer.apply_gradients(zip(grad_disc, discriminator.trainable_variables))
-        gen_optimizer.apply_gradients(zip(grad_gen, generator.trainable_variables))
+        disc_optimizer.apply_gradients(
+            zip(grad_disc, discriminator.trainable_variables))
+        gen_optimizer.apply_gradients(
+            zip(grad_gen, generator.trainable_variables))
 
         return gen_loss, disc_loss
 
     checkpoint_dir = './training_checkpoints'
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-    checkpoint = tf.train.Checkpoint(generator_optimizer=gen_optimizer,discriminator_optimizer=disc_optimizer,
-                                    generator=generator,discriminator=discriminator)
+    checkpoint = tf.train.Checkpoint(generator_optimizer=gen_optimizer, discriminator_optimizer=disc_optimizer,
+                                     generator=generator, discriminator=discriminator)
 
     seed = tf.random.normal([16, latent_dim])
     kl_history = []
@@ -205,17 +226,19 @@ def main(arg):
         total_disc_loss = 0
 
         for batch_x, batch_y in train_dataset:
-            gen_loss, disc_loss = train_step(batch_x,batch_y)
+            gen_loss, disc_loss = train_step(batch_x, batch_y)
             total_gen_loss += gen_loss
             total_disc_loss += disc_loss
 
         # if epoch % 10 == 0 :
         noise = tf.random.normal([len(x), latent_dim])
-        kl_div = calculate_kl_div(generator,noise,y.astype(np.float32),norm_p)
+        kl_div = calculate_kl_div(
+            generator, noise, y.astype(np.float32), norm_p)
         kl_history.append(kl_div)
         g_losses.append(total_gen_loss / batch_size)
         disc_losses.append(total_disc_loss / batch_size)
-        print('Time for epoch {:01.0f} is {:.5} sec - gen_loss = {:.5}, disc_loss = {:.5} kl_div = {:.5}'.format(epoch + 1, time.time() - start, total_gen_loss / batch_size, total_disc_loss / batch_size, kl_div))
+        print('Time for epoch {:01.0f} is {:.5} sec - gen_loss = {:.5}, disc_loss = {:.5} kl_div = {:.5}'.format(
+            epoch + 1, time.time() - start, total_gen_loss / batch_size, total_disc_loss / batch_size, kl_div))
 
         G_train_loss.reset_states()
         D_train_loss.reset_states()
@@ -231,13 +254,15 @@ def main(arg):
     H["kl_divergence"] = kl_history
 
     with open("./logs/CGAN_RELU.pickle", "wb") as output_file:
-        pickle.dump(H,output_file)
+        pickle.dump(H, output_file)
 
-    #Load Ml classifiers
+    # Load Ml classifiers
     ml_classifiers = utils.load_pretrained_classifiers()
     print('pretrained ml classifiers loaded : [OK]')
 
-    clf.compare(x,y.astype(np.float32), x_test[data_cols].values[for_test], y_test[for_test], generator, label_mapping, ml_classifiers ,cv=5,rand_noise_dim=latent_dim)
+    clf.compare(x, y.astype(np.float32), x_test[data_cols].values[for_test], y_test[for_test],
+                generator, label_mapping, ml_classifiers, cv=5, rand_noise_dim=latent_dim)
+
 
 if __name__ == '__main__':
     main(None)
--- NSL-KDD/test.py
+++ NSL-KDD/test.py
@@ -1,35 +1,36 @@
+from tensorflow.keras.models import load_model
+import models.classifiers as clf
+from models import cgan
+from utils import utils
+from utils import preprocessing
 import numpy as np
-import pickle, os
+import pickle
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style("darkgrid")
 
-from utils import preprocessing
-from utils import utils
-
-from models import cgan
-import models.classifiers as clf
-
-from tensorflow.keras.models import load_model
 
 def main():
-    #Load Data & Preprocess
-    train,test, label_mapping = preprocessing.get_data(encoding="Label")
-    data_cols = list(train.columns[ train.columns != 'label' ])
+    # Load Data & Preprocess
+    train, test, label_mapping = preprocessing.get_data(encoding="Label")
+    data_cols = list(train.columns[train.columns != 'label'])
 
-    #Remove contant values with a threshold
-    to_drop = preprocessing.get_contant_featues(train,data_cols,threshold=0.995)
+    # Remove contant values with a threshold
+    to_drop = preprocessing.get_contant_featues(
+        train, data_cols, threshold=0.995)
 
-    train.drop(to_drop, axis=1,inplace=True)
-    test.drop(to_drop, axis=1,inplace=True)
+    train.drop(to_drop, axis=1, inplace=True)
+    test.drop(to_drop, axis=1, inplace=True)
 
-    #Normalize row-wise the data (to unit norm) and scale column-wise
-    data_cols = list(train.columns[train.columns != 'label' ])
-    train = preprocessing.normalize_data(train,data_cols)
-    test = preprocessing.normalize_data(test,data_cols)
-    x_train , x_test = preprocessing.preprocess(train,test,data_cols,"Robust",True)
-    data_cols = list(x_train.columns[x_train.columns != 'label' ])
+    # Normalize row-wise the data (to unit norm) and scale column-wise
+    data_cols = list(train.columns[train.columns != 'label'])
+    train = preprocessing.normalize_data(train, data_cols)
+    test = preprocessing.normalize_data(test, data_cols)
+    x_train, x_test = preprocessing.preprocess(
+        train, test, data_cols, "Robust", True)
+    data_cols = list(x_train.columns[x_train.columns != 'label'])
 
     train, test = None, None
     y_train = x_train.label.values
@@ -41,24 +42,27 @@ def main():
     del label_mapping["normal"]
     clf.DISPLAY_PERFOMANCE = False
 
-    x = x_train[data_cols].values[att_ind] #x_train.query(f'label == {label_mapping["normal"]}').values
+    # x_train.query(f'label == {label_mapping["normal"]}').values
+    x = x_train[data_cols].values[att_ind]
     y = y_train[att_ind]
     x_train, y_train = None, None
     print('Data read and preprocess : [DONE]')
 
-    utils.PlotPCA(2,x,y,label_mapping)
+    utils.PlotPCA(2, x, y, label_mapping)
     print('PCA plot : [DONE]')
-    #Load Ml classifiers
+    # Load Ml classifiers
     ml_classifiers = utils.load_pretrained_classifiers()
     print('pretrained ml classifiers loaded : [OK]')
 
-    #load pretrained GAN generator model
+    # load pretrained GAN generator model
     model = load_model('./trained_generator/gen.h5')
 
     # Evaluate classifiers performance after balancing data with GAN or SMOTE
-    clf.compare(x,y, x_test[data_cols].values[for_test], y_test[for_test], model, label_mapping, ml_classifiers ,cv=5) #CGAN
+    clf.compare(x, y, x_test[data_cols].values[for_test], y_test[for_test],
+                model, label_mapping, ml_classifiers, cv=5)  # CGAN
     # for smoteMethod in ["ADASYN","SMOTEENN","BorderlineSMOTE", "SMOTE"]:
     #     clf.compare(x,y, x_test[data_cols].values[for_test], y_test[for_test], smoteMethod, label_mapping, ml_classifiers ,cv=5) #SMOTE
+
 
 if __name__ == '__main__':
     main()
